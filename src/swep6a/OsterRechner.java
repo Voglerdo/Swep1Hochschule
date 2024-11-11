eb4596daf2a1ed7dd0493a9cd9f647351e2b974e
Dominik <120841465+Voglerdo@users.noreply.github.com>
2024-11-11 16:04:36 +0100

    test commit
    test commit

--- src/swep6a/OsterRechner.java
+++ src/swep6a/OsterRechner.java
@@ -3,6 +3,26 @@ package swep6a;
 import uebungen3A.InputTools;
 
 public class OsterRechner {
+	public static void main(String args[]) {
 	int j = InputTools.readInteger("geben sie das Jahr ab ");
-
+	int a;
+	int b;
+	int m;
+	int n;
+	int q;
+	int w; 
+	int p;
+	
+	
+	n = j-1900;
+	a = n % 19;
+	b = (7*a+1); // idiv 19
+	m = (11*a+4-b) % 29;
+	q= n; // idiv 4 
+	w=(n+q+31+m)%7;
+	p= 25-m-w;
+	
+	System.out.println(p);
+	
+}
 }
