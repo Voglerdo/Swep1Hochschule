edfa67a40cecdc1c88ac23f48b2acddb4c2f708f
Dominik <120841465+Voglerdo@users.noreply.github.com>
2024-11-18 14:17:43 +0100

    aufgabe6

--- src/swep6a/TemperaturRechner.java
+++ src/swep6a/TemperaturRechner.java
@@ -4,6 +4,24 @@ import uebungen3A.InputTools;
 
 public class TemperaturRechner {
 	public static void main(String args[]) {
-	double input = InputTools.readDouble("geben sie das Jahr ab ");
-}
+	String umrechnungsWeg = InputTools.readString("Umrechnungsrichtung");
+	
+	if(umrechnungsWeg.equals("1")){
+		umrechnungsWeg= "Celsius zu Fahrenheit";
+	}
+	else {
+		umrechnungsWeg= "Fahrenheit zu Celsius";
+	}
+	
+	double input = InputTools.readDouble(umrechnungsWeg);
+	
+	if (umrechnungsWeg.equals("Celsius zu Fahrenheit")) {
+		 double fahrenheit = (9.0 / 5.0) * input + 32;
+         System.out.printf("%.2f°C entsprechen %.2f°F%n", input, fahrenheit);
+	}
+	else {
+		double celsius = (input - 32) * 5.0 / 9.0;
+        System.out.printf("%.2f°F entsprechen %.2f°C%n", input, celsius);
+	}
+	}
 }
