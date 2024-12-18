bec6b59380e350e27d7029cf0f54d2f025b1117e
Dominik <120841465+Voglerdo@users.noreply.github.com>
2024-12-18 22:45:44 +0100

    11

--- src/swepb11/A.java
+++ src/swepb11/A.java
@@ -2,7 +2,10 @@ package swepb11;
 
 public class A {
 
-public boolean addB(B b) {
+public boolean addB(B newB) {
+	return false;
+}
+public boolean removeB(B old) {
 	return false;
 }
 }
--- src/swepb11/B.java
+++ src/swepb11/B.java
@@ -1,5 +1,15 @@
 package swepb11;
 
 public class B extends A{
+private int random ;
+
+public B() {
+	super();
+}
+
+public int getRandomInt() {
+	return 0;
+}
+
 
 }
